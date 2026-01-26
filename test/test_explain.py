7e402d0d585d22aec98a7620f461b17d8b9f2b0f
yang.wang <yang.wang@smartnews.com>
2026-01-26 08:00:34 +0000

    update tests

--- test/test_explain.py
+++ test/test_explain.py
@@ -4,8 +4,9 @@ from threading import Thread
 
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 
-sys.path.append(os.path.dirname(__file__))
-from utils import print_param_stats, print_cuda_memory, print_model_precision
+#sys.path.append(os.path.dirname(__file__))
+from test.utils import print_param_stats, print_cuda_memory, print_model_precision
+from test.data_titles import titles, results
 
 model_name = "OpenOneRec/OneRec-8B"
 
@@ -20,42 +21,7 @@ print_param_stats(model)
 print_cuda_memory()
 print_model_precision(model)
 
-#promp = "这是一个视频：<|sid_begin|><s_a_340><s_b_6566><s_c_5603><|sid_end|>，帮我总结一下这个视频讲述了什么内容"
-#promp = "这是一个视频：<|sid_begin|><s_a_340><s_b_6566><s_c_5603><|sid_end|>，帮我用英语总结一下这个视频讲述了什么内容"
-#promp = "这是一个视频：<|sid_begin|><s_a_340><s_b_6566><s_c_5603><|sid_end|>，帮我用日语总结一下这个视频讲述了什么内容"
-
-# "15元毛巾评测",
-#promp = "这是一个视频：<|sid_begin|><s_a_6037><s_b_2395><s_c_4831><|sid_end|>，帮我总结一下这个视频讲述了什么内容"
-
-# "快手爆款：10分钟学会糖醋里脊做法"
-#promp = "这是一个视频：<|sid_begin|><s_a_5993><s_b_7362><s_c_6071><|sid_end|>，帮我总结一下这个视频讲述了什么内容" # mean + l2
-#promp = "这是一个视频：<|sid_begin|><s_a_5993><s_b_1638><s_c_5241><|sid_end|>，帮我总结一下这个视频讲述了什么内容" # mean
-promp = "这是一个视频：<|sid_begin|><s_a_5993><s_b_7362><s_c_1871><|sid_end|>，帮我总结一下这个视频讲述了什么内容" # last + l2
-promp = "这是一个视频：<|sid_begin|><s_a_5993><s_b_1638><s_c_368><|sid_end|>，帮我总结一下这个视频讲述了什么内容" # last
-
-#promp = "这是一个新闻：<|sid_begin|><s_a_8038><s_b_4887><s_c_857><|sid_end|>，帮我总结一下这个新闻讲述了什么内容"
-#promp = "这是SmartNews上的一则新闻：<|sid_begin|><s_a_2610><s_b_1283><s_c_2477><|sid_end|>，帮我总结一下这个新闻讲述了什么内容"
-
-# "半導体大手が来年度の設備投資を増額へ。AI需要の拡大を背景に生産能力を強化する。",
-# promp = "这是SmartNews上的一则新闻：<|sid_begin|><s_a_2779><s_b_5195><s_c_6071><|sid_end|>，帮我总结一下这个新闻讲述了什么内容"
-# "気象庁は週末にかけて広い範囲で大雨の可能性があるとして早めの備えを呼びかけた。"
-#promp = "这是SmartNews上的一则新闻：<|sid_begin|><s_a_4885><s_b_4716><s_c_6071><|sid_end|>，帮我总结一下这个新闻讲述了什么内容"
-
-messages = [
-    {"role": "user", "content": promp}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True
-)
-# -> added prefix & suffix to text
-
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-# -> model_inputs['input_ids'] is a tensor of shape [1, seq_len]
-
-def generate_sync():
+def generate_sync(model_inputs):
     generated_ids = model.generate(
             **model_inputs,
             max_new_tokens=4096
@@ -74,7 +40,7 @@ def generate_sync():
     print("=== Content ===")
     print(content)
 
-def generate_async():
+def generate_async(model_inputs):
     streamer = TextIteratorStreamer(
         tokenizer,
         skip_prompt=True,
@@ -88,12 +54,34 @@ def generate_async():
     thread = Thread(target=model.generate, kwargs=generation_kwargs)
     thread.start()
 
-    print("=== Thinking ===")
     for next in streamer:
+        if next == "<think>":
+            print("=== Thinking ===")
         print(next, end="", flush=True)
         if next == "</think>":
             print("\n=== Content ===")
     print()
     thread.join()
 
-generate_async()
+#promp = "这是一个视频：<|sid_begin|><s_a_340><s_b_6566><s_c_5603><|sid_end|>，帮我总结一下这个视频讲述了什么内容"
+
+template = "这是日文版SmartNews上的一则新闻：{}，帮我总结一下这个新闻讲述了什么内容"
+#template = "这是日文版SmartNews上用户最近浏览的的一则新闻：{}，根据这条新闻帮我生成三个新的用户可能感兴趣的候选新闻，用来推荐给用户。并解释每个新闻的内容。"
+
+for title, sid in zip(titles, results):
+    promp = template.format(sid)
+    messages = [
+        {"role": "user", "content": promp}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+        enable_thinking=False
+    )
+    # -> added prefix & suffix to text
+
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+    # -> model_inputs['input_ids'] is a tensor of shape [1, seq_len]
+    print("Title:", title)
+    generate_async(model_inputs)
--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -1,12 +1,14 @@
 
-import torch  # 第三方：PyTorch（张量/模型推理）
-from transformers import AutoModel, AutoTokenizer  # 第三方：Hugging Face Transformers（加载 tokenizer/模型）
+import torch
+from transformers import AutoModel, AutoTokenizer
 from transformers.utils.hub import cached_file
-from test.utils import l2_normalize, mean_pool, last_hidden_pool
+
+from test.utils import l2_normalize, mean_pool
 from tokenizer.res_kmeans import ResKmeans
 
-MODEL_ID = "Qwen/Qwen3-Embedding-8B"  # 必须用这个 embedding 模型来对齐 OneRec
-BATCH_SIZE = 1  # 小一点更省显存；大一点更快
+from test.data_titles import titles
+MODEL_ID = "Qwen/Qwen3-Embedding-8B"  
+BATCH_SIZE = 1
 
 
 def format_sid(codes: list[int]) -> str:
@@ -15,22 +17,6 @@ def format_sid(codes: list[int]) -> str:
 
 
 def main() -> None:
-    texts = [
-        #"15元毛巾评测",
-        "快手爆款：10分钟学会糖醋里脊做法",
-        #"农村老家的晚霞太美了，带你看看今天的天空", # <|sid_begin|><s_a_1371><s_b_4120><s_c_7314><|sid_end|>
-        #"街头魔术挑战：30秒让路人猜不透", # <|sid_begin|><s_a_802><s_b_7825><s_c_3919><|sid_end|>
-        #"健身小白一周打卡记录，看看变化有多大",
-        #"一起听老歌：90年代经典情歌合集",
-        #"政府は春の補正予算案を発表し、子育て支援と地方の交通インフラに重点配分するとした。",
-        #"都内で新型の電気バスが運行開始。静音性と充電効率の向上が評価されている。",
-        #"半導体大手が来年度の設備投資を増額へ。AI需要の拡大を背景に生産能力を強化する。",
-        #"気象庁は週末にかけて広い範囲で大雨の可能性があるとして早めの備えを呼びかけた。",
-        #"人気アイドルグループが新曲を公開し、音楽配信チャートで初登場1位を獲得した。",
-        #"映画祭で話題作が最優秀作品賞に輝き、主演俳優が壇上で感謝の言葉を述べた。",
-        #"有名アニメの続編が制作決定。ティザー映像が公開されSNSで大きな反響を呼んだ。",
-    ]
-
     # load Qwen embedding model
     tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
     model = AutoModel.from_pretrained(
@@ -61,11 +47,11 @@ def main() -> None:
     device = "cuda" if torch.cuda.is_available() else "cpu"
     sid_model = sid_model.to(device).eval()
 
-    for text in texts:
-        model_inputs = tokenizer([text], return_tensors="pt").to(model_device)
+    for title in titles:
+        model_inputs = tokenizer([title], return_tensors="pt").to(model_device)
         model_output = model(**model_inputs)
-        emb = mean_pool, last_hidden_pool(model_output.last_hidden_state, model_inputs["attention_mask"])
-        emb = l2_normalize(emb)
+        emb = mean_pool(model_output.last_hidden_state, model_inputs["attention_mask"])
+        # emb = l2_normalize(emb)
 
         emb = torch.tensor(emb, dtype=torch.float32, device=device)
         with torch.no_grad():
@@ -76,14 +62,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
-
-"""
-<|sid_begin|><s_a_8038><s_b_4887><s_c_857><|sid_end|>
-<|sid_begin|><s_a_2610><s_b_1283><s_c_2477><|sid_end|>
-<|sid_begin|><s_a_2779><s_b_5195><s_c_6071><|sid_end|>
-<|sid_begin|><s_a_4885><s_b_4716><s_c_6071><|sid_end|>
-<|sid_begin|><s_a_1353><s_b_3042><s_c_6071><|sid_end|>
-<|sid_begin|><s_a_2304><s_b_7438><s_c_2477><|sid_end|>
-"""
